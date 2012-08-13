f1f75840187dca0a722d57131e9a15c9fb79ed32
Simon Hughes <simon@hicrest.net>
2012-08-13 11:12:15 +0100

    Added assembly versioning

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,9 +10,9 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("Effortless.Net.Encryption")]
 [assembly: AssemblyDescription("Effortless .Net Encryption is a library that provides: Rijndael encryption/decyption, Hashing and Digest creation/validation and Password/salt creation.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Simon Hughes")]
+[assembly: AssemblyCompany("Simon Hughes, www.hicrest.net")]
 [assembly: AssemblyProduct("Effortless.Net.Encryption")]
-[assembly: AssemblyCopyright("Copyright © Simon Hughes 2008")]
+[assembly: AssemblyCopyright("Copyright © Simon Hughes 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -25,16 +25,3 @@ using System.Runtime.CompilerServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("a7c08577-d381-4530-8181-18533cef95c0")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
