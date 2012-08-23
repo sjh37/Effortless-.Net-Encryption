f272a489faac10299c749f7c87926e251f1a3719
Simon Hughes <simon.hughes@bybox.com>
2012-08-23 22:52:39 +0100

    Incremented version

--- Properties/VersionInfo.base.cs
+++ Properties/VersionInfo.base.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("1.0.4.$REVNUM$")]
-[assembly: AssemblyFileVersion("1.0.4.$REVNUM$")]
+[assembly: AssemblyVersion("1.0.5.$REVNUM$")]
+[assembly: AssemblyFileVersion("1.0.5.$REVNUM$")]
