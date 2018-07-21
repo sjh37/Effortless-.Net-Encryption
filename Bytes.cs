3d5241b23b99a0e1b6325562672e33b5d6a7d2c5
Simon Hughes <simon@hicrest.net>
2018-07-21 18:17:10 +0100

    Correct code indentation

--- Bytes.cs
+++ Bytes.cs
@@ -419,37 +419,37 @@ namespace Effortless.Net.Encryption
             }
         }
 
-/// <summary>
-/// Converts HEX string to btye array.
-/// Opposite of ByteArrayToHex.
-/// </summary>
-public static byte[] HexToByteArray(string hexString)
-{
-    if (hexString == null) throw new ArgumentNullException(nameof(hexString));
+        /// <summary>
+        /// Converts HEX string to btye array.
+        /// Opposite of ByteArrayToHex.
+        /// </summary>
+        public static byte[] HexToByteArray(string hexString)
+        {
+            if (hexString == null) throw new ArgumentNullException(nameof(hexString));
 
-    if ((hexString.Length % 2) != 0)
-        throw new ApplicationException("Hex string must be multiple of 2 in length");
+            if ((hexString.Length % 2) != 0)
+                throw new ApplicationException("Hex string must be multiple of 2 in length");
 
-    var byteCount = hexString.Length / 2;
-    var byteValues = new byte[byteCount];
-    for (var i = 0; i < byteCount; i++)
-    {
-        byteValues[i] = Convert.ToByte(hexString.Substring(i * 2, 2), 16);
-    }
+            var byteCount = hexString.Length / 2;
+            var byteValues = new byte[byteCount];
+            for (var i = 0; i < byteCount; i++)
+            {
+                byteValues[i] = Convert.ToByte(hexString.Substring(i * 2, 2), 16);
+            }
 
-    return byteValues;
-}
+            return byteValues;
+        }
 
-/// <summary>
-/// Convert bytes to 2 hex characters per byte, "-" separators are removed.
-/// Opposite of HexToByteArray
-/// </summary>
-public static string ByteArrayToHex(byte[] data)
-{
-    if (data == null) throw new ArgumentNullException(nameof(data));
+        /// <summary>
+        /// Convert bytes to 2 hex characters per byte, "-" separators are removed.
+        /// Opposite of HexToByteArray
+        /// </summary>
+        public static string ByteArrayToHex(byte[] data)
+        {
+            if (data == null) throw new ArgumentNullException(nameof(data));
 
-    return BitConverter.ToString(data).Replace("-", "");
-}
+            return BitConverter.ToString(data).Replace("-", "");
+        }
 
         /// <summary>
         /// Use cryptographically strong random number generator to fill buffer with random data.
