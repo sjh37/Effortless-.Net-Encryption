775a5ec07e32490649eabf73ca49a681235ba554
Simon Hughes <simon@hicrest.net>
2019-10-18 13:06:39 +0100

    Use newer msbuild exe

--- pack.bat
+++ pack.bat
@@ -1,3 +1,3 @@
-"C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" /t:Build  /p:Configuration="Release";Platform="any cpu"
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" /t:Build  /p:Configuration="Release";Platform="any cpu"
 del *.nupkg /Q
 nuget pack -properties Configuration=Release
