663d19e25144a832927e444552bf4b29f3a2338f
Simon Hughes <simon@hicrest.net>
2024-04-30 17:07:46 +0100

    Update to .Net 8

--- pack.bat
+++
@@ -1,3 +0,0 @@
-"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\amd64\MSBuild.exe" /t:Build  /p:Configuration="Release";Platform="any cpu"
-del *.nupkg /Q
-nuget pack -properties Configuration=Release
